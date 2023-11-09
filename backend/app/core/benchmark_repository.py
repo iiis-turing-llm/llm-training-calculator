47d29d8bcdc304e49e0608881bddb59dcd84d3d0
唐文鹏 <>
2023-11-09 10:39:23 +0800

    feat: modify read_benchmark_file logic

--- backend/app/core/benchmark_repository.py
+++ backend/app/core/benchmark_repository.py
@@ -6,22 +6,20 @@ class BenchmarkRepository:
     def read_benchmark_file(self, content):
         data = []
         current_object = []
-        is_valid_object = False  # 用于标记是否当前对象是完整的
+        has_start = False
         reader = csv.reader(content)
         for row in reader:
-            if row[0] == "iteration start":
-                if current_object:  # 如果上一个对象不完整，则舍弃
-                    if is_valid_object:
-                        data.append(current_object)
-                current_object = [(row[0], row[1])]  # 创建新对象
-                is_valid_object = False  # 重置对象有效标记
-            elif row[0] == "iteration end":
-                current_object.append((row[0], row[1]))  # 加入"iteration end"
-                is_valid_object = True  # 标记对象为完整
+            if row[0] == 'iteration start':
+                current_object = [(row[0], row[1])]
+                has_start = True
+            elif row[0] == 'iteration end':
+                current_object.append((row[0], row[1]))
+                if has_start:
+                    data.append(current_object)
+                    current_object = []
+                    has_start = False
             else:
-                current_object.append((row[0], row[1]))  # 加入其他行的数据
-        if current_object and is_valid_object:  # 处理最后一个对象
-            data.append(current_object)
+                current_object.append((row[0], row[1]))
 
         print(data)
         return data
