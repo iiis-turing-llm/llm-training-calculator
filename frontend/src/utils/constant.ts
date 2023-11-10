c0cd681324a4c487772a30ec6a625a0c2678f900
xuhuanhuan <tianlaixhh@163.com>
2023-11-10 15:29:17 +0800

    fix: recommendation coverage problem

--- frontend/src/utils/constant.ts
+++ frontend/src/utils/constant.ts
@@ -1,3 +1,3 @@
 export const repo_url = "https://github.com/iiis-turing-llm";
-// export const service_base_url = (<any>window).service_base_url || 'http://localhost:8000' // when develop
-export const service_base_url = (<any>window).service_base_url || '' // when deploy
+export const service_base_url = (<any>window).service_base_url || 'http://localhost:8000' // when develop
+// export const service_base_url = (<any>window).service_base_url || '' // when deploy
