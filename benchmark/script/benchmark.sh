cc1fa2339809aa3842159ecc873bcd4871a90567
ZeBraHack0 <git@github.com>
2023-12-22 10:54:49 +0800

    upgrade to v4.0

--- benchmark/script/benchmark.sh
+++ benchmark/script/benchmark.sh
@@ -21,9 +21,22 @@ show_usage() {
 }
 
 
+install(){
+    local version=${1} 
+    case ${version} in
+        "v3")
+            install_v3.0
+            ;;
+        "v4")
+            install_v4.0
+            ;;
+        *)
+            show_usage
+            ;;
+    esac
+}
 
-
-install() {
+install_v3.0() {
     echo_back "cd ${SHELL_FOLDER}"
     echo_info "install megatron v3.02"
     echo_back "wget --no-check-certificate --content-disposition https://github.com/NVIDIA/Megatron-LM/archive/refs/tags/v3.0.2.zip"
@@ -36,16 +49,41 @@ install() {
     local _sw_list=(__init__.py schedules.py global_vars.py training.py)
     for _item in ${_sw_list[@]}
     do
-        echo_back "cp script/${_item} Megatron/megatron/"
+        echo_back "cp script/megatron4.0/${_item} Megatron/megatron/"
     done
 }
 
-setup() {
+install_v4.0() {
     echo_back "cd ${SHELL_FOLDER}"
-    echo_back "cd ../Megatron"
+    echo_info "install megatron v4.0"
+    echo_back "wget --no-check-certificate --content-disposition https://github.com/NVIDIA/Megatron-LM/archive/refs/tags/core_v0.4.0.zip"
+    echo_back "unzip Megatron-LM-core_v0.4.0.zip"
+    echo_back  "cd ../"
+    echo_back "mkdir Megatron"
+    echo_back "mv script/Megatron-LM-core_v0.4.0/*  Megatron"
+    echo_back "rm -r script/Megatron-LM-core_v0.4.0/"
+    echo_info "install tracer"
+    local _sw_list=(__init__.py global_vars.py training.py)
+    for _item in ${_sw_list[@]}
+    do
+        echo_back "cp script/megatron4.0/${_item} Megatron/megatron/"
+    done
+    echo_back "cp script/megatron4.0/schedules.py Megatron/megatron/core/pipeline_parallel/"
+}
+
+setup() {
+    local version=${1} 
+    local prefix_dir=""
+    if [${version} == "v3"]; then 
+        prefix_dir="megatron3.0"
+    else
+        prefix_dir="megatron4.0"
+    fi
+    echo_back "cd ${SHELL_FOLDER}"
+    echo_back "cd ../megatron"
     echo_info "we only provide a small dataset for correctness validation"
     echo_back "cp -r ../datasets ./"
-    echo_back "cp ../examples/* examples/"
+    echo_back "cp ../examples/${prefix_dir}/* examples/"
     mkdir_if_not_exist ngc_models
     echo_back "bash examples/pretrain_gpt_distributed_dataprep_small.sh"
 }
@@ -53,7 +91,7 @@ setup() {
 train() {
     echo_back "cd ${SHELL_FOLDER}"
     echo_info "adjust parameters in the pretrain bash before training"
-    echo_back "cd ../Megatron"
+    echo_back "cd ../megatron"
     echo_info "clear checkpoint"
     rmdir_if_exist ngc_models/release_gpt_base
     echo_back "bash examples/pretrain_gpt_distributed_small.sh"
@@ -75,10 +113,10 @@ username=`whoami | awk '{print $1}'`
 global_choice=${1}
 case ${global_choice} in
     "install")
-        install
+        install ${2}
         ;;
     "setup")
-        setup
+        setup ${2}
         ;;
     "train")
         train
